5853d0040c545ba64354b2797034de9290d59587
radwa <clumsyshark@gmail.com>
2018-03-24 18:14:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Parrot Bot
+===
+Simple telegram bot that sends message to [parrot telegram channel](https://t.me/pmacaw) about [packtpub free learning](https://www.packtpub.com/packt/offers/free-learning)
---
+++ index.js
@@ -0,0 +1,33 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const secret = require('./secret')
+
+const packt_uri = `https://www.packtpub.com/packt/offers/free-learning`;
+const telegram_uri = `https://api.telegram.org/bot${secret.auth_token}/sendMessage`;
+
+const packt_options = {
+  uri: `${packt_uri}`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+rp(packt_options)
+  .then(($) => {
+    book_title = $('.dotd-title').text().trim();
+    if(book_title)
+      rp.post({
+        uri: `${telegram_uri}`,
+        body: {
+            chat_id: '@pmacaw',
+            text: `Today's book from packtpub: ${book_title} \n Get it now at ${packt_uri}`,
+            disable_web_page_preview: true
+        },
+        json: true 
+    })
+    else
+      console.log(`Book title is empty`)
+  })
+  .catch((err) => {
+    console.log(err);
+});	
