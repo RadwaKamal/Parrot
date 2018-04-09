f2b1ba40c479dd193c1e4dec720ee34fabf5910c
radwa <clumsyshark@gmail.com>
2018-04-09 14:36:16 +0200

    Show remaining time in msg

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ setInterval(() => {
   rp(packt_options)
     .then(($) => {
       book_title = $('.dotd-title').text().trim();
+      countdown_to = $('.packt-js-countdown').data('countdown-to');
+      remaining_seconds = countdown_to - Math.floor(Date.now() / 1000);
+      time_remaining = simpleFormatter(remaining_seconds)
       if(book_title)
         rp.post({
           uri: `${telegram_uri}`,
           body: {
               chat_id: '@pmacaw',
-              text: `Today's book from packtpub: ${book_title} \n Get it now at ${packt_uri}`,
+              text: `Today's book from packtpub: ${book_title} \ntime remaining: ${time_remaining}\nGet it now at ${packt_uri}`,
               disable_web_page_preview: true
           },
           json: true 
@@ -32,3 +35,16 @@ setInterval(() => {
       console.log(err);
   });	
 }, 88200000)
+
+let simpleFormatter = (seconds) => {
+  seconds = parseInt(seconds);
+  let hh = Math.floor(seconds / 3600);
+  let mm = Math.floor((seconds - (hh * 3600)) / 60);
+  let ss = seconds - (hh * 3600) - (mm * 60);
+
+  if (hh < 10) {hh = '0' + hh}
+  if (mm < 10) {mm = '0' + mm}
+  if (ss < 10) {ss = '0' + ss}
+
+  return hh + ':' + mm + ':' + ss;
+};
